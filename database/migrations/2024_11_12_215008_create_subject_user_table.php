53eba681f81648d7509adec0387d19ccbb5c3539
percivalyan <yoryan8888@gmail.com>
2024-12-01 19:19:16 +0700

    main: Added new migration

---
+++ database/migrations/2024_11_12_215008_create_subject_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subject_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Foreign key for users table
+            $table->foreignId('subject_id')->constrained('subjects')->onDelete('cascade'); // Foreign key for subjects table
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject_user');
+    }
+};
